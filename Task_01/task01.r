88c550aba7cd663ba1e78008217df04bf1326339
makaylaball <mb0270@mix.wvu.edu>
2022-01-14 11:54:41 -0500

    sshd

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -1,3 +1,35 @@
 # pw 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
 #install.packages ("swirl")
 library (swirl)
+swirl ()
+Makayla Ball
+1
+1
+1
+1
+5 + 7
+x <- 5 + 7
+x
+y=x-3
+y <- x - 3
+y
+c(1.1, 9, 3.14)
+z <- c(1.1, 9, 3.14)
+?c
+z
+?c
+z <- c(555)
+c(z, 555, z)
+z * 2 + 1000
+my_sqrt <- c(z-1)
+my_sqrt <- sqrt(z - 1)
+1
+my_sqrt
+my_div <- z / my_sqrt
+2
+my_div
+c(1, 2, 3, 4, 0, 10)
+c(1, 2, 3, 4) + c(0, 10)
+c(1, 2, 3, 4) + c(0, 10, 100)
+my_div
+1
